6813b1e1c40a3c04dc995cbcab19815f4fa6a881
Muhd Dandarawie Bin Rohani <s10208266@connect.np.edu.sg>
2020-12-27 16:36:53 +0800

    working search and image display

--- search.js
+++ search.js
@@ -1,3 +1,4 @@
+/*
 $(document).ready(function () {
     var settings = {
       "url": "https://pokeapi.co/api/v2/pokemon/"+search+"/",
@@ -9,3 +10,37 @@ $(document).ready(function () {
     });
   
   });//eof documentready
+*/
+
+  $("#poke_form").submit(function(event){
+    event.preventDefault()
+
+    $("#poke_image").empty()
+
+    var search_poke = $("#search_poke").val()
+
+    var url = "https://pokeapi.co/api/v2/pokemon/"+search_poke+"/"
+
+
+    // var $poke_image = $('#poke_image');
+
+    $.ajax({
+      
+      method:'GET',
+      url:url,
+      success:function(data){
+        console.log(data)
+        $('#poke_image').html('');
+
+        $('#poke_image').append('<li class="list-image"> <img src="'+data.sprites.front_default+'"></li>');
+
+        /*
+        $.each(data, function(i,picture){
+            $poke_image.append('<li>'+picture.name+'</li>');  
+
+        })
+        */
+      }
+    })
+  })
+
