8f1e60892508e84eb69850e3ba3d385ede6d3a3e
Muhd Dandarawie Bin Rohani <s10208266@connect.np.edu.sg>
2020-12-27 16:40:23 +0800

    explaining purpose of codes

--- search.js
+++ search.js
@@ -1,4 +1,4 @@
-/*
+/* Test codes
 $(document).ready(function () {
     var settings = {
       "url": "https://pokeapi.co/api/v2/pokemon/"+search+"/",
@@ -10,19 +10,21 @@ $(document).ready(function () {
     });
   
   });//eof documentready
+  End of test codes
 */
 
   $("#poke_form").submit(function(event){
-    event.preventDefault()
 
-    $("#poke_image").empty()
+    event.preventDefault() //prevent auto submission
+
+    $("#poke_image").empty() //upon submission, values in website deleted automatically
 
     var search_poke = $("#search_poke").val()
 
-    var url = "https://pokeapi.co/api/v2/pokemon/"+search_poke+"/"
+    var url = "https://pokeapi.co/api/v2/pokemon/"+search_poke+"/" //api and search function
 
 
-    // var $poke_image = $('#poke_image');
+    // Test code var $poke_image = $('#poke_image');
 
     $.ajax({
       
@@ -34,11 +36,13 @@ $(document).ready(function () {
 
         $('#poke_image').append('<li class="list-image"> <img src="'+data.sprites.front_default+'"></li>');
 
-        /*
+        /* 
+        Test codes
         $.each(data, function(i,picture){
             $poke_image.append('<li>'+picture.name+'</li>');  
 
         })
+        End of test codes
         */
       }
     })
